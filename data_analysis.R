fb4d4e97124ff619bc1aca664b6afd3561099478
SaucyBoi21 <72944967+SaucyBoi21@users.noreply.github.com>
2023-03-07 22:36:43 -0500

    added shiny functionality

--- data_analysis.R
+++ data_analysis.R
@@ -1,7 +1,10 @@
 library(tidyverse)
+library(shiny)
 
 plant_data <- read.csv("C:\\Users\\saaha\\OneDrive\\Documents\\GitHub\\BiomassPredictonAI\\harvest_bydate.csv")
 
 View(plant_data)
 
-ggplot(data = plant_data) + geom_point(mapping = aes(x = LFW_g, y = LDW_g))
+ggplot(data = plant_data) + geom_point(mapping = aes(x = plant_area, y = LDW_g))
+
+runExample("01_hello")
